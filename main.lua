fef031230e5bb6f077195d251278a952f14cb07e
Tad Thorley <phaedryx@gmail.com>
2014-02-24 20:53:44 -0700

    figure out centering dynamically

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 function love.draw()
-  font = love.graphics.newFont("assets/fonts/NovaMono.ttf", 72)
+  font   = love.graphics.newFont("assets/fonts/NovaMono.ttf", 72)
+  width  = love.window.getWidth()
+  height = love.window.getHeight()
   love.graphics.setFont(font)
-  love.graphics.printf('Hello World!', 0, 200, 800, 'center')
+  love.graphics.printf('Hello World!', 0, height/3, width, 'center')
 end
