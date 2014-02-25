6bc0d26ea65ceb61162748b2d3fdd4650336633d
Tad Thorley <phaedryx@gmail.com>
2014-02-24 21:05:07 -0700

    change the colors

--- main.lua
+++ main.lua
@@ -2,6 +2,9 @@ function love.draw()
   font   = love.graphics.newFont("assets/fonts/NovaMono.ttf", 72)
   width  = love.window.getWidth()
   height = love.window.getHeight()
+
   love.graphics.setFont(font)
+  love.graphics.setColor(0, 0, 0)
+  love.graphics.setBackgroundColor(255, 255, 255)
   love.graphics.printf('Hello World!', 0, height/3, width, 'center')
 end
